d60dfb07bc384bc9ddab3cca78265b5b6e79a4bb
lakssshhh <laksh69sharma@gmail.com>
2026-05-22 13:35:10 +0530

    Implement merge function for two sorted arrays

---
+++ 88. MergeSortedArray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                 swap(nums1[i], nums1[k]);
+                k--;
+                i--;
+            }else{
+                 swap(nums2[j], nums1[k]);
+                k--;
+                j--;
+
+            }
+        }
+        while (j >= 0) {
+        swap(nums2[j], nums1[k]);
+        j--;
+        k--;
+    }
+    }
+};
