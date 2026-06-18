dc5e81f59f89852fc12d50c1630e06f2e2942da5
lakssshhh <laksh69sharma@gmail.com>
2026-06-18 12:22:33 +0530

    Implement deleteNode function for linked list

---
+++ Code Testcase Testcase Test Result 237. Delete Node in a Linked List.cpp
@@ -0,0 +1,5 @@
+ void deleteNode(ListNode* node) {
+        ListNode* nextNode = node -> next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete nextNode;
