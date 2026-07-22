794a143446e67d3558260492392bf30c93b2d273
lakssshhh <laksh69sharma@gmail.com>
2026-07-22 12:59:01 +0530

    Create 225. Implement Stack using Queues.cpp

---
+++ 225. Implement Stack using Queues.cpp
@@ -0,0 +1,25 @@
+class MyStack {
+    queue<int> q;
+public:
+    void push(int x) {
+        q.push(x);
+        for (int i=1; i<q.size(); i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+       int val = q.front();
+        q.pop();
+        return val;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
