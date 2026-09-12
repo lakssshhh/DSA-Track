7d174de3fef5e200bb6fe87cc1fdb1b4ab6ed641
lakssshhh <laksh69sharma@gmail.com>
2026-09-12 13:00:18 +0530

    Create 45. Jump Game II.cpp

---
+++ 45. Jump Game II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+    int left = 0, right = 0, jump = 0;
+    while (right < nums.size() - 1) {
+        int farthest = 0;
+        for (int i = left; i <= right; i++) {
+            farthest = max(farthest, i + nums[i]);
+        }
+        left = right + 1;
+        right = farthest;
+        jump++;
+    }
+    
+    return jump;
+}
+};
