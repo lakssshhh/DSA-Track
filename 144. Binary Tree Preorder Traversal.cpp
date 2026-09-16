3f03c7f0b4726354b91f0c89aa8bcdd9c7ea8e50
lakssshhh <laksh69sharma@gmail.com>
2026-09-16 14:12:54 +0530

    Implement preorder traversal for binary tree

---
+++ 144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    solve(root, ans);
+    return ans;
+}
+void solve(TreeNode* root, vector<int>& ans) {
+    if (!root) {
+        return;
+    }
+    ans.push_back(root->val);
+    solve(root->left, ans);
+    solve(root->right, ans);
+}
+};
