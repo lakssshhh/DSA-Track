8f750274e22829442cfddc244cbd7705401d2665
lakssshhh <laksh69sharma@gmail.com>
2026-09-17 15:26:05 +0530

    Create 145. Binary Tree Postorder Traversal.cpp

---
+++ 145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    solve(root, ans);
+    return ans;
+    }
+    void solve(TreeNode* root, vector<int>& ans) {
+    if (!root) {
+        return;
+    }
+    solve(root->left,ans);
+    solve(root->right,ans);
+    ans.push_back(root->val);
+    }
+};
